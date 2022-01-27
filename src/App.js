84b072fa697e8428ea2b2790f298ef44ba761665
Jon <urbangentryjon@gmail.com>
2022-01-27 14:33:43 +0700

    first css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Heading</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+
+export default App;
